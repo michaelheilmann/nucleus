ebebce3371ab779b3604be96527e068c3231be8d
Michael Heilmann <michaelheilmann@primordialmachine.com>
2017-09-15 21:24:19 +0200

    Improve doc comments

--- src/nucleus-annotations.h
+++ src/nucleus-annotations.h
@@ -9,16 +9,52 @@
 /// @param ... list of 1-based parameter indices.
 /// If the parameter index of a pointer parameter is in that list, then this indicates that the pointer parameter expects a non-null pointer.
 /// An empty indicates that all pointer parameters expect non-null values.
+/// @details For example in following declaration this macro is used to annotate the parameters
+/// @a y and @a w as expecing non-null argument values.
+/// @code Spine_NonNull(2,4) void f(char *x, char *y, char *z, char *w); @endcode
+/// Note that the parameter indices are 1-based.
 #define Nucleus_NonNull(...)
 
+/// @brief Annotation for an input parameter variable.
+
+
+
 /// @brief A parameter annotation indicating a parameter is an input parameter.
+/// @details
+/// This macro can be used to indicate that a parameter is an output parameter.
+/// @param parameter the parameter declaration
+/// @remarks
+/// For example in the function declaration below this macro is used
+/// to annotate the parameter @a x as an input and output parameter.
+/// @code
+/// int f(Nucleus_InputParameter(int *x), int y);
+/// @endcode
 #define Nucleus_InputParameter(parameter) parameter
 
 /// @brief A parameter annotation indicating a parameter is an output parameter.
+/// @details
+/// This macro can be used to indicate that a parameter is an output parameter.
+/// @param parameter the parameter declaration
+/// @remarks
+/// For example in the following function declaration below this macro is used
+/// to annotate the parameter @a x as an output parameter.
+/// @code
+/// int f(Nucleus_OutputParameter(int *x), int y);
+/// @endcode
 #define Nucleus_OutputParameter(parameter) parameter
 
-/// @brief A paraeter annotation indicating a parameter is an input and output parameter.
+/// @brief A parameter annotation indicating a parameter is an input and output parameter.
+/// @details
+/// This macro can be used to indicate that a parameter is an output parameter.
+/// @param parameter the parameter declaration
+/// @remarks
+/// For example in the following function declaration below this macro is used
+/// to annotate the parameter @a x as an input and output parameter.
+/// @code
+/// int f(Nucleus_InputOutputParameter(int *x), int y);
+/// @endcode
 #define Nucleus_InputOutputParameter(parameter) parameter
 
-/// @brief Alias for @a lx_input_output_parameter.
+/// @brief Alias for @code{Nucleus_InputOutputParameter}.
+/// @param parameter the parameter declaration
 #define Nucleus_OutputInputParameter(parameter) parameter
