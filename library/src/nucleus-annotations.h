34ced6557d837609de1f1f748ef2123a64ec58db
Michael Heilmann <michaelheilmann@primordialmachine.com>
2017-09-18 02:33:43 +0200

    Improve doc annotations comments

--- library/src/nucleus-annotations.h
+++ library/src/nucleus-annotations.h
@@ -1,26 +1,32 @@
 /// @file nucleus-annotations.h
-/// @brief Function and parameter annotations.
+/// @brief Function, parameter, and expression annotations.
 #pragma once
 
 #include "nucleus-config.h"
 
+/// @defgroup Annotations annotations
+/// @brief Function, parameter, and expression annotations.
+
+/// @ingroup Annotations
 /// @brief A function annotation indicating that the function never raises an error.
 #define Nucleus_NoError()
 
-/// @brief A function annotation which allow to indicate that pointer parameters expect non-null pointers.
+/// @ingroup Annotations
+/// @brief A function annotation which allows to indicate that pointer parameters expect non-null pointers.
 /// @param ... list of 1-based parameter indices.
 /// If the parameter index of a pointer parameter is in that list, then this indicates that the pointer parameter expects a non-null pointer.
 /// An empty indicates that all pointer parameters expect non-null values.
 /// @details For example in following declaration this macro is used to annotate the parameters
 /// @a y and @a w as expecing non-null argument values.
-/// @code Nucleus_NonNull(2,4) void f(char *x, char *y, char *z, char *w); @endcode
+/// @code Spine_NonNull(2,4) void f(char *x, char *y, char *z, char *w); @endcode
 /// Note that the parameter indices are 1-based.
 #if defined(Nucleus_Compiler_GCC)
-	#define Nucleus_NonNull(...) __attribute__((nonnull (__VA_ARGS__)))
+	#define Nucleus_NonNull(...) __attribute__((nonnull (__VA_ARGS__))) /**< @hideinitializer */
 #else
 	#define Nucleus_NonNull(...)
 #endif
 
+/// @ingroup Annotations
 /// @brief A parameter annotation indicating a parameter is an input parameter.
 /// @details
 /// This macro can be used to indicate that a parameter is an output parameter.
@@ -31,8 +37,9 @@
 /// @code
 /// int f(Nucleus_InputParameter(int *x), int y);
 /// @endcode
-#define Nucleus_InputParameter(parameter) parameter
+#define Nucleus_InputParameter(parameter) parameter /**< @hideinitializer */
 
+/// @ingroup Annotations
 /// @brief A parameter annotation indicating a parameter is an output parameter.
 /// @details
 /// This macro can be used to indicate that a parameter is an output parameter.
@@ -43,8 +50,9 @@
 /// @code
 /// int f(Nucleus_OutputParameter(int *x), int y);
 /// @endcode
-#define Nucleus_OutputParameter(parameter) parameter
+#define Nucleus_OutputParameter(parameter) parameter /**< @hideinitializer */
 
+/// @ingroup Annotations
 /// @brief A parameter annotation indicating a parameter is an input and output parameter.
 /// @details
 /// This macro can be used to indicate that a parameter is an output parameter.
@@ -55,8 +63,9 @@
 /// @code
 /// int f(Nucleus_InputOutputParameter(int *x), int y);
 /// @endcode
-#define Nucleus_InputOutputParameter(parameter) parameter
+#define Nucleus_InputOutputParameter(parameter) parameter /**< @hideinitializer */
 
+/// @ingroup Annotations
 /// @brief Alias for Nucleus_InputOutputParameter.
 /// @param parameter the parameter declaration
-#define Nucleus_OutputInputParameter(parameter) parameter
+#define Nucleus_OutputInputParameter(parameter) parameter /**< @hideinitializer */
